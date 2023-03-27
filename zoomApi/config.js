7d6e786f451be001b25825d425d91257d54d730d
Shiwang Gupta <guptashiwang02@gmail.com>
2023-03-27 23:18:46 +0530

    config.js api keys update

--- zoomApi/config.js
+++ zoomApi/config.js
@@ -3,8 +3,8 @@ const env = process.env.NODE_ENV || "production";
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
   development: {
-    APIKey: "G8ciugfoRqGf5m_vZ8NETQ",
-    APISecret: "qVNsNI0LJ5jygqOv7djeA61MrkMMP0minyvF",
+    APIKey: "Your API KEY",
+    APISecret: "YOUR API SECRET KEY",
   },
   production: {
     APIKey: "G8ciugfoRqGf5m_vZ8NETQ",
